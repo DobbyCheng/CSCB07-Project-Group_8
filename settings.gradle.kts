a197bdc75882667153282e07c0882f744ef21773
yuchengcheng <cycdobby@gmail.com>
2023-11-24 16:59:34 -0500

    modified Student Comment and Rating

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Comment and rating for event"
+include(":app")
