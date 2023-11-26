53b94fe199a73e63898e53cd57fdb2227319a0e2
yuchengcheng <cycdobby@gmail.com>
2023-11-25 22:56:08 -0500

    modified CheckPOSt.java and Driver.java

--- CheckPOSt.java
+++ CheckPOSt.java
@@ -1,87 +1,118 @@
 package checkPOSt;
 
-import java.util.Scanner;
-
 public class CheckPOSt {
 	
-	static Scanner input = new Scanner(System.in);
+	double credit;
+	int grade31, grade67, grade08, grade22, grade37, grade48;
+	String adm;
+	boolean completeCourses;
 	
-	public CheckPOSt() {}
-	
-	public void printResult(boolean res) {
-		System.out.println(res?"Qualified for POSt!":"Not qualified for POSt");
+	public boolean checkvalid1(String c) {
+		try {
+			credit = Double.parseDouble(c);
+		}catch(Exception e){
+			return false;
+		}
+		return true;
 	}
 	
-	public String admissionCategory() {
-		System.out.println("Admission category (CS/Math/Stats/Other):");
-		String s = input.next();
-		return s;
+	public void getCompleteCourses(boolean f) {
+		completeCourses = f;
 	}
 	
-	public void admCS() {
-		System.out.println("Grade point average across the following five "
-					+ "courses: CSC/MAT A67, CSC A48, MAT A22, MAT A31, MAT A37:");
-		double pointAverage = input.nextDouble();
-		if(pointAverage < 2.5) {
-			System.out.println("Not admitted to CS Major/Specialist");
-			return;
+	public String checkMinor() {
+		if(completeCourses) return "Qualified for POSt!";
+		else return "Not qualified for POSt";
+	}
+	
+	public boolean checkvalid2(String g1, String g2, String g3,
+			String g4, String g5, String g6, String s) {
+		try {
+			grade31 = Integer.parseInt(g1);
+			grade67 = Integer.parseInt(g2);
+			grade08 = Integer.parseInt(g3);
+			grade22 = Integer.parseInt(g4);
+			grade37 = Integer.parseInt(g5);
+			grade48 = Integer.parseInt(g6);
+			adm = s;
+		}catch(Exception e){
+			return false;
 		}
-		System.out.println("Is your grade in CSC A48 at least B? (Yes/No)");
-		String s = input.next();
-		if(s.equals("No")) {
-			System.out.println("Not admitted to CS Major/Specialist");
-			return;
+		if(grade31 < 0 || grade31 > 100 || grade67 < 0 || grade67 > 100
+				|| grade08 < 0 || grade08 > 100 || grade22 < 0 
+				|| grade22 > 100 || grade37 < 0 || grade37 > 100
+				|| grade48 < 0 || grade48 > 100) {
+			return false;
 		}
-		System.out.println("Is your grade in two of CSC/MAT A67, MAT A22, "
-				+ "MAT A37 at least C- ? (Yes/No)");
-		s = input.next();
-		if(s.equals("Yes")) {
-			System.out.println("Admitted to CS Major/Specialist!");
+		return true;
+	}
+	
+	public String checkSpecialist() {
+		if(adm == "CS") {
+			if(admCS()) return "Admitted to CS Specialist";
+			else return "Not admitted to CS Specialist";
+		}else if(adm == "MS") {
+			if(admMS()) return "Qualified for POSt";
+			else return "Not qualified for POSt";
 		}else {
-			System.out.println("Not admitted to CS Major/Specialist");
+			if(admOther()) return "Qualified for POSt";
+			else return "Not qualified for POSt";
 		}
 	}
 	
-	public boolean completeCourses() {
-		System.out.println("Have you completed CSC/MAT A67, CSC A48, "
-				+ "MAT A22, MAT A37? (Yes/No)");
-		String s = input.next();
-		return s.equals("Yes");
-	}
-	
-	public void admMS() {
-		printResult(completeCourses());
-	}
-	
-	public void admOther() {
-		if(!completeCourses()) {
-			printResult(false);
-			return;
-		}
-		System.out.println("Do you get at least an A- in both CSC/MAT A67 "
-				+ "and MAT A31 the first time that you complete "
-				+ "those courses? (Yes/No)");
-		String s = input.next();
-		printResult(s.equals("Yes"));
-	}
-	
-	public void checkMajor() {
-		String adm = admissionCategory();
-		if(adm.equals("CS")) {
-			admCS();
-		}else if(adm.equals("Other")) {
-			admOther();
-		}else{
-			admMS();
+	public String checkMajor() {
+		if(adm == "CS") {
+			if(admCS()) return "Not admitted to CS Major";
+			else return "Admitted to CS Major";
+		}else if(adm == "MS") {
+			if(admMS()) return "Qualified for POSt";
+			else return "Not qualified for POSt";
+		}else {
+			if(admOther()) return "Qualified for POSt";
+			else return "Not qualified for POSt";
 		}
 	}
 	
-	public void checkMinor() {
-		System.out.println("Have you taken all required A-level CSC "
-				+ "and MAT courses (CSCA08, CSCA48, and one of "
-				+ "CSCA67/MATA67, MATA22/A23, MATA30/A31/A32)?(Yes/No):");
-		String s = input.next();
-		printResult(s.equals("Yes"));
+	public double convertGrade(int x) {
+		if(x>=85) return 4.0;
+		else if(x>=80) return 3.7;
+		else if(x>=77) return 3.3;
+		else if(x>=73) return 3.0;
+		else if(x>=70) return 2.7;
+		else if(x>=67) return 2.3;
+		else if(x>=63) return 2.0;
+		else if(x>=60) return 1.7;
+		else if(x>=57) return 1.3;
+		else if(x>=53) return 1.0;
+		else if(x>=50) return 0.7;
+		else return 0.0;
 	}
 	
+	public boolean admCS() {
+		if(credit < 4.0) return false;
+		double sum = convertGrade(grade31) + convertGrade(grade67) + 
+				convertGrade(grade08) + convertGrade(grade22) +
+				convertGrade(grade37) + convertGrade(grade48);
+		double pointAverage = (double)sum / 6.0;
+		System.out.println(pointAverage);
+		if(pointAverage < 2.5) {
+			return false;
+		}
+		if(grade48 < 73) {
+			return false;
+		}
+		int num = 0;
+		if(grade67 >= 60) ++num;
+		if(grade22 >= 60) ++num;
+		if(grade37 >= 60) ++num;
+		return num >= 2;
+	}
+	
+	public boolean admMS() {
+		return credit >= 4.0;
+	}
+	
+	public boolean admOther() {
+		return credit >= 4.0 && grade67 >= 80 && grade31 >= 80;
+	}
 }
--- Driver.java
+++ Driver.java
@@ -1,24 +1,10 @@
 package checkPOSt;
 
-import java.util.Scanner;
-
 public class Driver {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Credits completed:");
-		double credits = input.nextDouble();
-		if(credits < 4.0) {
-			System.out.println("Not qualified for POSt");
-			input.close();
-			return;
-		}
-		
-		System.out.println("Specialist/Major/Minor:");
-		String s = input.next();
-		CheckPOSt ch = new CheckPOSt();
-		if(s.equals("Minor")) ch.checkMinor();
-		else ch.checkMajor();
-		input.close();
+		CheckPOSt x = new CheckPOSt();
+		x.checkvalid1("4.0");
+		x.checkvalid2("100", "100", "100", "100", "66", "73", "CS");
+		System.out.println(x.checkSpecialist());
 	}
 }
